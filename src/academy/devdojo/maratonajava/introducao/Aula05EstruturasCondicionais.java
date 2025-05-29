1ee91002d35f7c37bc89aa329348d2f529ca877b
Gerard Ferrer <ferrergerard112@gmail.com>
2025-05-29 10:05:43 -0300

    segundo comit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 10;
+        boolean autorizado = idade > 18;
+         if (autorizado){
+             System.out.println("Permitido");
+         }
+         else {
+             System.out.println("Não autorizado seu vadio");
+         }
+
+         int idadefut = 10;
+
+         if (idadefut < 15) {
+             System.out.println("Categoria infantil");
+         }
+         else if (idadefut >= 15 && idadefut<18) {
+             System.out.println("Categoria juvenil");
+
+         }
+         else{
+             System.out.println("Categoria adulto");
+         }
+
+    }
+}
