1b83f17b45ed75c0dd88aa08312e882d0f903aa1
Gerard Ferrer <ferrergerard112@gmail.com>
2025-07-14 15:32:01 -0300

    teste

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -10,11 +10,10 @@ confirmo que recebi o salario de <Salario>, na data <Data>
 public class Aula03TiposPrimitivosExercicio {
     public static void main(String[] args) {
         String nome = "Gerard";
-        String endereco = "pará, castanhal, centro, senador lemos N220";
-        double salario = 2000;
-        String dataDoRecebimento = "27/05/2025";
-        String mesagem = ("Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salario de " + salario
-                +", na data " + dataDoRecebimento);
-        System.out.println(mesagem);
+        String endereco = "Senador antonio  lemos, castanhal, Pará";
+        String data = "14/07/2025";
+        double salario = 2500.99;
+        String mensagem = "Eu "+nome+" morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data;
+            System.out.println(mensagem);
     }
 }
