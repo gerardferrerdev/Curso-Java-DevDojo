b2d84cf5852066f5d69fc623594e8291f10ff287
Gerard Ferrer <ferrergerard112@gmail.com>
2025-07-15 01:13:09 -0300

    utilizando o while para fazer uma tabuada simples

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    //Imprima os primeiros 25 numeros de um dado valor
+    public static void main(String[] args) {
+
+    }
+}
