f68b6b2be1819443b8378d58886963acccb67469
Gerard Ferrer <ferrergerard112@gmail.com>
2025-07-16 03:22:43 -0300

    Arrays multidimensionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        int [][] dia = new int [3][2];
+        dia [0][0]= 1;
+        dia [0][1]= 2;
+        dia [1][0]= 4;
+        dia [1][1]= 5;
+        dia [2][0]= 7;
+        dia [2][1]= 8;
+                for (int i = 0; i < dia.length ; i++) {
+            for (int j = 0; j < dia [0].length ; j++) {
+                System.out.println(dia[i][j]);
+            }
+
+        }
+    }
+}
