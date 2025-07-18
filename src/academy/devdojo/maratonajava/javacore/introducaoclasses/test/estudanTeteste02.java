0470314471015e8694cde415b2dad3f0dc1f489e
Gerard Ferrer <ferrergerard112@gmail.com>
2025-07-18 00:22:32 -0300

    introdução a progrmação orienada a objetos

--- src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/estudanTeteste02.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/estudanTeteste02.java
@@ -6,7 +6,7 @@ import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.estudante
 public class estudanTeteste02 {
     public static void main(String[] args) {
         estudante estudante = new estudante();
-        System.out.println(estudante.nome);
+        System.out.println(estudante.nomje);
     }
 
 }
